169bafcbf3636e7f245133133c504e12f85259f2
faridBOD <fhosman@videoamp.com>
2020-11-15 10:01:39 -0500

    Adds build

---
+++ build/precache-manifest.690b9c3d02a206fb4e915396b76319c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f9f11072f366075832178642354146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa99ad55bf5397eacbce",
+    "url": "/static/css/main.0a0929e3.chunk.css"
+  },
+  {
+    "revision": "7e7808bedd9b159dbd47",
+    "url": "/static/js/2.8012e7cb.chunk.js"
+  },
+  {
+    "revision": "0d9e118d1fe2d45db9aaf6e07593b98a",
+    "url": "/static/js/2.8012e7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa99ad55bf5397eacbce",
+    "url": "/static/js/main.b8b0e27b.chunk.js"
+  },
+  {
+    "revision": "7358fdfe4c2bf556f30d",
+    "url": "/static/js/runtime-main.b7804788.js"
+  }
+]);
